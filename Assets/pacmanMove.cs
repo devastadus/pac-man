0dc17d7074ae1f8943223f06d81cdcc78bdda1e8
Scott Price <scottprice7@gmail.com>
2016-05-31 22:11:04 -0700

    updates

---
+++ Assets/pacmanMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pacmanMove : MonoBehaviour
+{
+    public float speed = 0.4f;
+    Vector2 dest = Vector2.zero;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    dest = transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void FixedU1pdate ()
+	{
+	    Vector2 p = Vector2.MoveTowards(transform.position, dest, speed);
+        GetComponent<Rigidbody2D>().MovePosition(p); 
+
+	}
+
+    bool valid(Vector2 dir)
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
+
+    }
+}
